0772a1f3440f2813dbb4a2aca2df9c8aa59c3c74
khanhnguyen2102003 <127201068+khanhnguyen2102003@users.noreply.github.com>
2023-09-09 19:16:01 +0700

    SearchAlgorithm.java

--- Controller/SearchAlgorithm.java
+++ Controller/SearchAlgorithm.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Dai Nhan
- */
 public class SearchAlgorithm {
 
     public int linerSearch(int[] arr, int input) {
