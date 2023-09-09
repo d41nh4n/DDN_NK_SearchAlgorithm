5e49411a868d879a034fb0d293bd277f7c19f54b
d41nh4n <d41nh4n@gmail.com>
2023-09-09 09:54:27 +0700

    Code Soure

---
+++ Controller/SearchAlgorithm.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Dai Nhan
+ */
+public class SearchAlgorithm {
+    
+    public void linerSearch(int[] arr, int input){
+        for(int i = 0; i < arr.length; i++){
+            if(input == arr[i]){
+                System.out.println("Found "+input+" at index: "+i);
+            }
+        }
+    }
+    
+    public void 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
