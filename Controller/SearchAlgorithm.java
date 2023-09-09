9b3f2df74b4f470b2cfba150155462db38aa857c
d41nh4n <d41nh4n@gmail.com>
2023-09-09 19:03:06 +0700

    Add full function

--- Controller/SearchAlgorithm.java
+++ Controller/SearchAlgorithm.java
@@ -4,137 +4,64 @@
  */
 package Controller;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Dai Nhan
  */
 public class SearchAlgorithm {
-    
-    public void linerSearch(int[] arr, int input){
-        for(int i = 0; i < arr.length; i++){
-            if(input == arr[i]){
-                System.out.println("Found "+input+" at index: "+i);
+
+    public int linerSearch(int[] arr, int input) {
+       
+        for (int i = 0; i < arr.length; i++) {
+            if (input == arr[i]) 
+            return i;
+        }
+        return -1;
+    }
+
+    public int binarySearch(int[] arr, int left, int right, int number) {
+        //Binary Sort
+//        ArrayList<Integer> n = null;
+        if (right >= left) {
+            // chia 2 mảng
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == number) {
+//                n.add(mid);
+                return mid;
+            }
+
+            if (arr[mid] <  number) {
+                return binarySearch(arr, mid + 1, right, number);
+            }
+
+            if (arr[mid] > number) {
+                return binarySearch(arr, left, mid - 1, number);
             }
         }
+//        if(n != null){
+//            leftNumber(arr, n.get(0) - 1, n.get(0), n);
+//            rightNumber(arr, n.get(0) + 1, n.get(0), n);
+//            return n;
+//        }
+
+        return -1;
     }
-    
-    public void 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-} 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    public ArrayList<Integer> leftNumber(int[] arr, int leftMid, int mid , ArrayList<Integer> n){
+//        if(leftMid == mid){
+//           n.add(mid);
+//           return leftNumber(arr, leftMid - 1, leftMid, n);
+//        }
+//        return n;
+//    }
+//    
+// public ArrayList<Integer> rightNumber(int[] arr, int rightMid, int mid , ArrayList<Integer> n){
+//        if(rightMid == mid){
+//           n.add(mid);
+//           return leftNumber(arr, rightMid + 1, rightMid, n);
+//        }
+//        return n;
+//    }
+}
---
+++ Controller/UseSearchAlgorithm.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Model.Element;
+import View.Menu;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dai Nhan
+ */
+public class UseSearchAlgorithm extends Menu<String> {
+
+    private Element element;
+    private SearchAlgorithm searchAlgorithm;
+    private SortAlgorithm sortAlgorithm;
+
+    public UseSearchAlgorithm() {
+        super("Programming", new String[]{
+            "Search", "Exit"
+        });
+        searchAlgorithm = new SearchAlgorithm();
+        sortAlgorithm = new SortAlgorithm();
+        element = new Element();
+    }
+
+    public int numberArray() {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("How many number in array(1-20): ");
+            try {
+                String input = sc.nextLine();
+                n = Integer.parseInt(input);
+                if (n >= 1 && n <= 20) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Input number from 1 to 20");
+            }
+        }
+        return n;
+    }
+
+    public void createElementByRandom(int n) {
+        int[] arr = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(100);
+        }
+        element.setArrayNumber(arr);
+        element.setSizeOfArray(n);
+    }
+//===========================================================================
+
+    @Override
+    public void execute() {
+        display();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        input = sc.nextLine();
+        switch (input) {
+            case "1":
+                search();
+                break;
+            case "2":
+                System.out.println("Exit...");
+                break;
+            default:
+                System.out.println("Wrong choice");
+        }
+    }
+
+    public void search() {
+        int number = numberArray();
+        createElementByRandom(number);
+        int[] arr = element.getArrayNumber();
+        System.out.println("Your random array number");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        String[] mcSearch = {"Linear Search", "Binary Search", "Exit"};
+        Menu menu = new Menu("Search Menu", mcSearch) {
+            @Override
+            public void execute() {
+                Scanner sc = new Scanner(System.in);
+                String input;
+                input = sc.nextLine();
+                switch (input) {
+                    case "1":
+
+                        System.out.println("Input number to search: ");
+                        int numberSearch = sc.nextInt();
+                        int numberfounded = searchAlgorithm.linerSearch(element.getArrayNumber(), numberSearch);
+                        if (numberfounded == -1) {
+                            System.out.println("Through linear search your number input not exist in array!");
+                        } else {
+                            System.out.println("Your input number located in position " + numberfounded + " of array");
+                        }
+                        break;
+                    case "2":
+                        element.setArrayNumber(sortAlgorithm.quickSort(element.getArrayNumber(), 0, number - 1));
+                        System.out.println("Your array number after sort");
+                        for (int i = 0; i < arr.length; i++) {
+                            System.out.println(arr[i]);
+                        }
+                        System.out.println("=======================");
+                        System.out.println("Input number to search: ");
+                        int numberSearch1 = sc.nextInt();
+                        int numberFounded1 = searchAlgorithm.binarySearch(arr, 0, arr.length - 1, numberSearch1);
+                        if (numberFounded1 == -1) {
+                            System.out.println("Through linear search your number input not exist in array!");
+                        } else {
+                            System.out.println("Your input number located in position " + numberFounded1 + " of array");
+                        }
+
+                        break;
+                    case "3":
+                        System.out.println("Exit...");
+                        break;
+                    default:
+                        System.out.println("Wrong choice");
+                }
+
+            }
+        };
+        menu.run();
+    }
+}
